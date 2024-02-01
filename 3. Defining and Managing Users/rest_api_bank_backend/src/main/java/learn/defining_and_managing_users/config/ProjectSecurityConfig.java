4a88f58fde466d8b071980890b323cf050472035
Sean <seanwhs@gmail.com>
2024-02-01 19:42:16 +0800

    Store user credentials in MySQL and use JdbcUserDetailsManager

--- 3. Defining and Managing Users/rest_api_bank_backend/src/main/java/learn/defining_and_managing_users/config/ProjectSecurityConfig.java
+++ 3. Defining and Managing Users/rest_api_bank_backend/src/main/java/learn/defining_and_managing_users/config/ProjectSecurityConfig.java
@@ -1,21 +1,21 @@
 //ProjectSecurityConfig.java
 package learn.defining_and_managing_users.config;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
     
     @Bean
-    SecurityFilterChain defauFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
         
             http.authorizeHttpRequests((request) -> request
             .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards").authenticated()
@@ -26,20 +26,9 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    //Configure users using InMemoeryUserDetailsManager
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails admin = User.withUsername("admin")
-            .password("pass")
-            .authorities("admin")
-            .build();
-
-        UserDetails user = User.withUsername("user")
-            .password("pass")
-            .authorities("read")
-            .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
