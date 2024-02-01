4fe194455e7fbfe5e745cf538be7331d2f56f5b5
Sean <seanwhs@gmail.com>
2024-02-01 16:20:38 +0800

    Create users using InMemoryUserDetailsManager and PasswordEncoder Bean

--- 3. Defining and Managing Users/rest_api_bank_backend/src/main/java/learn/defining_and_managing_users/config/ProjectSecurityConfig.java
+++ 3. Defining and Managing Users/rest_api_bank_backend/src/main/java/learn/defining_and_managing_users/config/ProjectSecurityConfig.java
@@ -3,10 +3,11 @@ package learn.defining_and_managing_users.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,18 +29,21 @@ public class ProjectSecurityConfig {
     //Configure users using InMemoeryUserDetailsManager
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails admin = User.withDefaultPasswordEncoder()
-            .username("admin")
+        UserDetails admin = User.withUsername("admin")
             .password("pass")
             .authorities("admin")
             .build();
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user")
+        UserDetails user = User.withUsername("user")
             .password("pass")
             .authorities("read")
             .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
