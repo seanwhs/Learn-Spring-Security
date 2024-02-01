697ae2c8267511d71ceb0736055ce794265c062f
Sean <seanwhs@gmail.com>
2024-02-01 21:29:35 +0800

    Implemented a RestAPI to allow users to register

--- 3. Defining and Managing Users/rest_api_bank_backend/src/main/java/learn/defining_and_managing_users/config/ProjectSecurityConfig.java
+++ 3. Defining and Managing Users/rest_api_bank_backend/src/main/java/learn/defining_and_managing_users/config/ProjectSecurityConfig.java
@@ -1,14 +1,11 @@
 //ProjectSecurityConfig.java
 package learn.defining_and_managing_users.config;
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,20 +14,17 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
         
-            http.authorizeHttpRequests((request) -> request
-            .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards").authenticated()
-            .requestMatchers("/notices", "/contact").permitAll())
-            .formLogin(Customizer.withDefaults())
-            .httpBasic(Customizer.withDefaults());
+            http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards").authenticated()
+                .requestMatchers("/notices", "/contact", "/register").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         
         return http.build();
     }
 
-    // @Bean
-    // public UserDetailsService userDetailsService(DataSource dataSource){
-    //     return new JdbcUserDetailsManager(dataSource);
-    // }
-
+ 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
