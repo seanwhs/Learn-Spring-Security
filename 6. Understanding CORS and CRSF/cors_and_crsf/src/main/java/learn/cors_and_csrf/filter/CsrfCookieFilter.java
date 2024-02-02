0c268114e5af243be84d0d7c826a3aa5f5b38139
seanwhs <seanwhs@gmail.com>
2024-02-02 21:35:45 +0800

    added full csrf solution

---
+++ 6. Understanding CORS and CRSF/cors_and_crsf/src/main/java/learn/cors_and_csrf/filter/CsrfCookieFilter.java
@@ -0,0 +1,27 @@
+//CsrfCookieFilter.java
+package learn.cors_and_csrf.filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class CsrfCookieFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @SuppressWarnings("null") HttpServletRequest request,
+            @SuppressWarnings("null") HttpServletResponse response,
+            @SuppressWarnings("null") FilterChain filterChain)
+            throws ServletException, IOException {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (null != csrfToken.getHeaderName()) {
+            response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
--- 6. Understanding CORS and CRSF/cors_and_crsf/src/main/java/learn/cors_and_csrf/security/ProjectSecurityConfig.java
+++ 6. Understanding CORS and CRSF/cors_and_crsf/src/main/java/learn/cors_and_csrf/security/ProjectSecurityConfig.java
@@ -1,51 +1,66 @@
 //ProjectSecurityConfig.java
 package learn.cors_and_csrf.security;
+
 import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import jakarta.servlet.http.HttpServletRequest;
+import learn.cors_and_csrf.filter.CsrfCookieFilter;
 
 @Configuration
 public class ProjectSecurityConfig {
-    
+
     @Bean
     SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
 
-            http.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
-                @Override
-                public CorsConfiguration getCorsConfiguration(@SuppressWarnings("null") HttpServletRequest request) {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                    config.setAllowedMethods(Collections.singletonList("*"));
-                    config.setAllowCredentials(true);
-                    config.setAllowedHeaders(Collections.singletonList("*"));
-                    config.setMaxAge(3600L);
-                    return config;
-                }
-            }));
-            
-            http
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext((context) -> context
+                .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+
+        http.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(@SuppressWarnings("null") HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L);
+                return config;
+            }
+        }));
+
+        http
                 .authorizeHttpRequests((request) -> request
-                .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards", "/user").authenticated()
-                .requestMatchers("/notices", "/contact", "/register").permitAll())
+                        .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards", "/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
-            http.csrf(csrf -> csrf.ignoringRequestMatchers("/contact", "/register"));
-        
+        http.csrf((csrf) -> csrf
+                .csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/register")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+
         return http.build();
     }
 
- 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
