e17b25f05d0f4377d7c89b09115263167cd1ce8e
seanwhs <seanwhs@gmail.com>
2024-02-02 19:50:14 +0800

    Added CORS configuration to allow communication with localhost:3000, defaiult origin for REACT apps

--- 6. Understanding CORS and CRSF/cors_and_crsf/src/main/java/learn/cors_and_csrf/security/ProjectSecurityConfig.java
+++ 6. Understanding CORS and CRSF/cors_and_crsf/src/main/java/learn/cors_and_csrf/security/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 //ProjectSecurityConfig.java
 package learn.cors_and_csrf.security;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,13 +8,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class ProjectSecurityConfig {
     
     @Bean
     SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
-        
+
+            http.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(@SuppressWarnings("null") HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }
+            }));
+            
             http
                 .authorizeHttpRequests((request) -> request
                 .requestMatchers("/myAccount", "/myLoans", "/myBalance", "/myCards", "/user").authenticated()
